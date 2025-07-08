5042352dff70be5f592f9c41a85e915ea55edeed
iam-aadityasingh <adityasinghnavimumbai@gamil.com>
2025-07-08 17:00:50 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h1 className='text-2xl text-bold text-pink-950'>Tradenest</h1>
+    </div>
+  )
+}
+
